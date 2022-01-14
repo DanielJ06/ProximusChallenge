373107daa69b3ba099fc861d922af9267d4382a5
danieljesus <daniel.j.rodrigues06@gmail.com>
2022-01-14 11:32:43 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider as StoreProvider, useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components/native";
+import "react-native-get-random-values";
+
+import { useEffect } from "react";
+import MainRoutes from "./src/routes/main.routes";
+import reactotron from "./src/config/reactotron";
+import store from "./src/store";
+import { ThemesEnum } from "./src/utils/uikit/themesEnum";
+import { Dark } from "./src/utils/uikit/themes/darkTheme";
+import { Light } from "./src/utils/uikit/themes/lightTheme";
+import { RootState } from "./src/utils/models/RootState";
+import { navigationRef } from "./src/utils/RootNavigation";
+
+const AppContent = () => {
+	const theme = useSelector((state: RootState) => state.theme);
+
+	return (
+		<ThemeProvider theme={theme.theme === ThemesEnum.dark ? Dark : Light}>
+			<NavigationContainer ref={navigationRef}>
+				<StatusBar style={theme.theme === ThemesEnum.dark ? "light" : "dark"} />
+				<MainRoutes />
+			</NavigationContainer>
+		</ThemeProvider>
+	);
+};
+
+export default function App() {
+	useEffect(() => {
+		if (__DEV__) {
+			reactotron.connect();
+		}
+	}, []);
+
+	return (
+		<StoreProvider store={store}>
+			<AppContent />
+		</StoreProvider>
+	);
+}
